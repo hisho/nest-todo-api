e836dd29b2e768d5ec85ecd59f91a4e0637d53ae
hisho <hisho.web@gmail.com>
2022-06-21 23:32:57 +0900

    ✅ e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { useContainer } from 'class-validator';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -12,13 +13,37 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    useContainer(app.select(AppModule), { fallbackOnErrors: true });
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/graphql (Todos)', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        query: `
+{
+  todo(uuid: "b87c9dee-9d61-40d3-9591-f99fc4f7ec56") {
+    createdAt
+    description
+    id
+    title
+    updatedAt
+    uuid
+  }
+}
+`,
+        variables: {
+          uuid: 'b87c9dee-9d61-40d3-9591-f99fc4f7ec56',
+        },
+      });
+    expect(response.ok).toBeTruthy();
+    expect(response.body.data.errors).toBeUndefined();
+    expect(response.body.data.todo).toHaveProperty('uuid');
+    expect(response.body.data.todo).toHaveProperty('id');
+    expect(response.body.data.todo).toHaveProperty('title');
+    expect(response.body.data.todo).toHaveProperty('description');
+    expect(response.body.data.todo).toHaveProperty('createdAt');
+    expect(response.body.data.todo).toHaveProperty('updatedAt');
   });
 });
